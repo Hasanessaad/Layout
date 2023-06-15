b32df7a49047ad53cb0462b301bc8f748b48bda6
hasan essaad <hasanessaad@gmail.com>
2023-06-15 18:11:13 -0300

    start-up

---
+++ vue-pro/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ESTACIONAMENTO</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/"><a class="nav-link active" aria-current="page" href="#">HOME</a></router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about"><a class="nav-link" href="#">MARCA</a></router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  <router-view/>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ vue-pro/src/views/AboutView.vue
@@ -0,0 +1,43 @@
+<template>
+
+  <div class="container text-center">
+    <div class="row">
+        <div class="col-8"><input class="put" type="filtro" placeholder="Nome da Especialidade"></div>
+      <div class="col-4"><button type="button" class="btn btn-primary btn-lg">Cadastrar</button>
+        <div class="btn-group">
+          <button type="button" class="btn btn-secondary dropdown-toggle btn-lg op-btn" data-bs-toggle="dropdown" aria-expanded="false"><!--an on click function should be added-->
+            opcoes
+          </button>
+          <ul class="dropdown-menu dropdown-menu-end">
+            <li><button class="dropdown-item" type="button">Action</button></li>
+            <li><button class="dropdown-item" type="button">Another action</button></li>
+            <li><button class="dropdown-item" type="button">Something else here</button></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style>
+.container{
+  margin-top: 20px;
+}
+.btn-options{
+  width: 800px;
+}
+.col-8{
+display: flex;
+align-items: center;
+justify-content: center;
+}
+.put{
+  width: 750px;
+  height: 40px;
+}
+.op-btn{
+  margin-left: 10px;
+}
+
+</style>
